2e3482b45be12d1e467b91f08a90c909ac946bb4
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-29 23:14:11 +0200

    Add main layout

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-const homeController = require('./controllers/homeController');//
 
-router.use(homeController);
+router.get('/', (req, res) => {
+    res.render('home');
+});
+
 
 module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
+const homeController = require('./controllers/homeController');
+router.use(homeController);
 
-router.get('/', (req, res) => {
-    res.render('home', { layout: false });
-});
+
 
 module.exports = router;
