723c8b3f862555d6db4c2337da9942dedd757b93
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-29 15:09:12 +0200

    Configure express handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
 
 const app = express();
-
 const port = 5000;
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine',)
+app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
     res.send('Home page');
 });
