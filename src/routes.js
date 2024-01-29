4d60a885500dda0460c2f2330926e79b9541fc18
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-29 23:44:02 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+
 const homeController = require('./controllers/homeController');
+const movieController = require('./controllers/movieController');
+
+router.use(movieController);
 router.use(homeController);
 
 
