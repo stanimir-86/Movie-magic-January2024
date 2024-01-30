502664288558788f240dc643b5adae8a9c042c89
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-30 15:48:44 +0200

    Add search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,6 +4,22 @@ exports.getAll = () => {
     return movies.slice();
 };
 
+exports.search = (title, genre, year) => {
+    let result = movies.slice();
+
+    if (title) {
+        result = result.filter(movie => movie.title.toLowerCase(title.toLoweCase()));
+    }
+    if (genre) {
+        result = result.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
+    }
+    if (genre) {
+        result = result.filter(movie => movie.year === year);
+    }
+
+    return result;
+}
+
 exports.getOne = (movieId) => {
     const movie = movies.find(movie => movie._id == movieId);
 
