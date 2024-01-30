11f0c78f2192ed34ee620a3c97fde3ec17d4b67c
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-30 10:09:06 +0200

    Update configExpress.js

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -4,7 +4,7 @@ const path = require('path');
 function configExpress(app) {
 
     app.use(express.static(path.resolve('src/public')));
-
+    app.use(express.urlencoded({ extended: false }));//вземане на данни динамично
     return app;
 }
 
