c5c9fb1ef61e583141f898363b8584aab7d75204
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-02 14:10:49 +0200

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,31 +1,26 @@
 const router = require('express').Router();
 
-const movieService = require('../services/movieService.js');
-
+const movieService = require('../services/movieService');
 
 router.get('/', async (req, res) => {
-
-    const movies = await movieService.getAll();
+    const movies = await movieService.getAll().lean();
 
     res.render('home', { movies });
 });
 
 router.get('/about', (req, res) => {
-
     res.render('about');
 });
 
-router.get('/search', (req, res) => {
-    const { title, genre, year } = req.query
-    const movieResult = movieService.search(title, genre, year);
+router.get('/search', async (req, res) => {
+    const { title, genre, year } = req.query;
+    const movies = await movieService.search(title, genre, year).lean();
 
-    res.render('search', { movies: movieResult, title, genre, year });
+    res.render('search', { movies, title, genre, year });
 });
 
 router.get('/404', (req, res) => {
     res.render('404');
 });
-
-
 
 module.exports = router;
