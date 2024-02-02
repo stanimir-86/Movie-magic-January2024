bece88f6bf47ab9db6acd741f7b2d4512ed87ba3
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-02 14:11:11 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
-const homeController = require('./controllers/homeController');
+const homeController = require('./controllers/homeController'); 
 const movieController = require('./controllers/movieController');
+const castController = require('./controllers/castController');
 
 router.use(homeController);
 router.use(movieController);
+router.use('/cast', castController);
 
-router.get('*', (req,res) => {
+router.get('*', (req, res) => {
     res.redirect('/404');
 });
 
