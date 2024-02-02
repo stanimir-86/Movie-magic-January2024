19cb8444bab6cf16d04b4c637014a25fd94792e5
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-02 14:10:44 +0200

    Update configHandlebars.js

--- src/config/configHandlebars.js
+++ src/config/configHandlebars.js
@@ -2,12 +2,13 @@ const handlebars = require('express-handlebars');
 const path = require('path');
 
 function configHandlebars(app) {
-
     app.engine('hbs', handlebars.engine({
         extname: 'hbs',
     }));
     app.set('view engine', 'hbs');
-    app.set('views',path.resolve('src/views'));
+    app.set('views', path.resolve('src/views'));
+
     return app;
 }
+
 module.exports = configHandlebars;
