b5dc7bf354a2bab72b5488be698f3fc1bd50f6de
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-02 14:10:52 +0200

    Update movieController.js

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-const movieService = require('../services/movieService.js');
+const movieService = require('../services/movieService');
+const castService = require('../services/castService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -13,18 +14,37 @@ router.post('/create', async (req, res) => {
         await movieService.create(newMovie);
 
         res.redirect('/');
-    } catch (error) {
-        console.log(error.message);
+    } catch (err) {
+        console.log(err.message);
         res.redirect('/create');
     }
-
-
 });
 
-router.get('/movies/:movieId', (req, res) => {
+router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
-    const movie = movieService.getOne(movieId);
+    const movie = await movieService.getOne(movieId).lean();
+    // const casts = await castService.getByIds(movie.casts).lean();
+
+    // TODO: This is not perfect, use handlebars helpers
     movie.rating = new Array(Number(movie.rating)).fill(true);
+
     res.render('details', { movie });
 });
+
+router.get('/movies/:movieId/attach', async (req, res) => {
+    const movie = await movieService.getOne(req.params.movieId).lean();
+    const casts = await castService.getAll().lean();
+    // TODO: remove already added casts
+    res.render('movie/attach', { ...movie, casts });
+});
+
+router.post('/movies/:movieId/attach', async (req, res) => {
+    const castId = req.body.cast;
+    const movieId = req.params.movieId;
+
+    await movieService.attach(movieId, castId);
+
+    res.redirect(`/movies/${movieId}/attach`);
+});
+
 module.exports = router;
