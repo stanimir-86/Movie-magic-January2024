62326ffbdfde56aafacc9e47c1b216c17cfcc836
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-03 13:54:46 +0200

    Update movieController.js

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -29,7 +29,7 @@ router.post('/create', isAuth, async (req, res) => {
 router.get('/movies/:movieId', isAuth, async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
-    const isOwner = movie.owner == req.user._id;
+    const isOwner = movie.owner == req.user?._id;
 
     // TODO: This is not perfect, use handlebars helpers
     movie.rating = new Array(Number(movie.rating)).fill(true);
